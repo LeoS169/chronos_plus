fb9a478179f8cbfdc60d37ad505151e1403c85b6
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-02 11:34:04 -0300

    att: class usuario

---
+++ chronos_pack/chronosClass/usuario.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+class Usuario:
+    def __init__(self, nome:str, email:str, senha:str):
+        self.nome = nome
+        self.email = email
+        self._senha = senha
+        self._data_criacao = date.today()
+        
+        
+    def __str__(self):
+        return f"""{self.__class__.__name__}: 
+    {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"""
+        
+
+    @classmethod
+    def cria_usuario(
+        cls,
+        nome:str,
+        email:str,
+        senha:str
+    ):
+        # Verifica se usuário existe
+        # Registra User em BD
+        pass
+    
+    
+    @property 
+    def senha(self):
+        return self.senha
+    
+    
+
+    
+        
