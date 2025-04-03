5ee893f0fd813119bffe35ebad952ac278ae906c
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-03 10:58:11 -0300

    att: troca do def retorna_usuario

--- chronos_pack/chronosClass/manipBD.py
+++ chronos_pack/chronosClass/manipBD.py
@@ -1,5 +1,6 @@
 import psycopg2 as pg2
 from json import load
+from verifBD import retorna_usuario
 
 with open("chronos_pack/chronosClass/connect.json", encoding="utf-8") as conexJson:
     db_conex = load(conexJson)
@@ -23,24 +24,6 @@ def registra_usuario(
         return 'Insert confirmed'
     except Exception as e:
         return e
-    finally:
-        cursor.close()
-        conex.close()
-        
-
-def retorna_usuario(email:str, senha:str):
-    try:
-        conex = pg2.connect(**db_conex)
-        cursor = conex.cursor()
-        
-        query = """
-            SELECT * FROM usuario WHERE email = %s AND senha = %s;
-        """
-        cursor.execute(query, (email, senha))
-        user = cursor.fetchone()
-        return 'Usuario encontrado', user
-    except Exception as e:
-        return e, ''
     finally:
         cursor.close()
         conex.close()
--- chronos_pack/chronosClass/verifBD.py
+++ chronos_pack/chronosClass/verifBD.py
@@ -63,3 +63,20 @@ def verify_diario(nome:str):
         cursor.close()
         conex.close()
         
+        
+def retorna_usuario(email:str, senha:str):
+    try:
+        conex = pg2.connect(**db_conex)
+        cursor = conex.cursor()
+        
+        query = """
+            SELECT * FROM usuario WHERE email = %s AND senha = %s;
+        """
+        cursor.execute(query, (email, senha))
+        user = cursor.fetchone()
+        return 'Usuario encontrado', user
+    except Exception as e:
+        return e, ''
+    finally:
+        cursor.close()
+        conex.close()
