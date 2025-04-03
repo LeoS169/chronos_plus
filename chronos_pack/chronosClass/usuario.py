14bd638a79c73ab02fc7b76947e130f7cbc7d467
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-03 09:29:57 -0300

    att: verificacao e registro de user na class Usuario

--- chronos_pack/chronosClass/usuario.py
+++ chronos_pack/chronosClass/usuario.py
@@ -1,4 +1,6 @@
 from datetime import date
+from verifBD import verify_usuario
+from manipBD import registra_usuario
 
 class Usuario:
     def __init__(self, nome:str, email:str, senha:str):
@@ -21,8 +23,22 @@ class Usuario:
         senha:str
     ):
         # Verifica se usuário existe
-        # Registra User em BD
-        pass
+        # Converte data_criacao para o BD
+        # Registra User no BD
+        user_existe = verify_usuario(email=email)
+        if not user_existe:
+            user = cls(nome, email, senha)
+            data_criacao = user._data_criacao.strftime('%Y-%m-%d')
+            
+            status = registra_usuario(
+                nome=nome,
+                email=email,
+                senha=senha,
+                data_criacao=data_criacao
+            )
+            return "Usuário criado", status
+        else:
+            return "Usuário já existe!", status
     
     
     def deletar(
@@ -31,6 +47,7 @@ class Usuario:
         senha:str
     ):
         # Verifica se usuário existe
+        
         # Deleta usuário
         pass
     
