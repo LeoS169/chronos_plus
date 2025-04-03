7cd609c6a854d0d01ae7eb3e3aa138fe129dde09
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-03 10:26:54 -0300

    att: manipBD add retorna_usuario + deleta_usuario

--- chronos_pack/chronosClass/manipBD.py
+++ chronos_pack/chronosClass/manipBD.py
@@ -20,12 +20,61 @@ def registra_usuario(
         """
         cursor.execute(query, (nome, email, senha, data_criacao,))
         conex.commit()
-        return 'ok'
+        return 'Insert confirmed'
     except Exception as e:
         return e
     finally:
         cursor.close()
         conex.close()
+        
+
+def retorna_usuario(email:str, senha:str):
+    try:
+        conex = pg2.connect(**db_conex)
+        cursor = conex.cursor()
+        
+        query = """
+            SELECT * FROM usuario WHERE email = %s AND senha = %s;
+        """
+        cursor.execute(query, (email, senha))
+        user = cursor.fetchone()
+        return 'Usuario encontrado', user
+    except Exception as e:
+        return e, ''
+    finally:
+        cursor.close()
+        conex.close()
+        
+
+def deleta_usuario(
+    email:str,
+    senha:str
+):
+    try:
+        conex = pg2.connect(**db_conex)
+        cursor = conex.cursor()
+        _, usuario = retorna_usuario(
+            email=email,
+            senha=senha
+        )
+        
+        if usuario:
+            query = """
+                DELETE FROM usuario WHERE email = %s AND senha = %s;
+            """
+            cursor.execute(query, (email, senha,))
+            conex.commit()
+            return 'Delete confirmed'
+        else:
+            return 'Erro: usuario não encontrado'
+        
+    except Exception as e:
+        return e
+    finally:
+        cursor.close()
+        conex.close()
+
+    
     
     
     
