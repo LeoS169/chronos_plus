e9753326bea98e3c4814494016f86c56db77b459
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-11 06:46:03 -0300

    docs: main.py funcao chronos_plus

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import flet as ft
 from pages.inicial_page import pagina_inicial
 
 def chronos_plus(page: ft.Page):
+    """
+    Inicializador Aplicativo
+    
+    Cria página com o Flet (ft.Page)
+    para ser inicializado. Usa a pagina
+    inicial como página de abertura
+    """
     pagina_inicial(page=page)
     
 
