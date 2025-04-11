86e51ff72cd6316945d50edf967da2a1da3e5f96
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-10 22:03:15 -0300

    add file: arquivo conv

---
+++ pages/conv.py
@@ -0,0 +1,21 @@
+from flet import (ElevatedButton, Text, 
+ButtonStyle, RoundedRectangleBorder)
+
+def cria_botao(texto:str):
+    return ElevatedButton(
+        content=Text(
+            texto,
+            font_family="Jersey15",
+            size=60
+        ),
+        style=ButtonStyle(
+            bgcolor="#222333",
+            color="white",
+            shape=RoundedRectangleBorder(radius=12),
+            elevation=5,
+            overlay_color="#2C2E51"
+        ),
+        width=400,
+         height=120,
+         on_click=None
+    )
