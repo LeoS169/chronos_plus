4e60473ecde3f3015381b779c3a6fed6125fd007
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-12 16:30:00 -0300

    att: eficiencia nos imports

--- pages/fluxo.py
+++ pages/fluxo.py
@@ -1,10 +1,15 @@
 from flet import Page
-from .entrar_page import pagina_entrar
-
 """
 Esse arquivo controla o fluxo entre as páginas
 """
 
+def voltar_pagina_inicial(page: Page):
+    from .inicial_page import pagina_inicial
+    page.clean()
+    pagina_inicial(page=page)
+    
+
 def go_entrar(page: Page):
+    from .entrar_page import pagina_entrar
     page.clean()
     pagina_entrar(page=page)
