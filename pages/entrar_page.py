1f8592bdd21ad99099a3be8b48372332229d7cbb
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-12 16:32:34 -0300

    att: adicao entradas_user

--- pages/entrar_page.py
+++ pages/entrar_page.py
@@ -1,10 +1,16 @@
-from flet import Row, Page, MainAxisAlignment, Text, app
-from .assets.tools.conv import cria_container
+from flet import(Row, Page, MainAxisAlignment,
+    Text, Column, ScrollMode, app)
+
+from .assets.tools.conv import (cria_container_elementos,
+    cria_container_entrada, cria_botao)
+
+from .fluxo import voltar_pagina_inicial 
 
 def pagina_entrar(page:Page):
     page.title = "Chronos+"
     page.horizontal_alignment = MainAxisAlignment.CENTER
     page.bgcolor = "#222333"
+    page.window.full_screen = True
     page.fonts = {
         "Jersey10": "pages/assets/fonts/Jersey10-Regular.ttf",
         "Jersey15": "pages/assets/fonts/Jersey15-Regular.ttf",
@@ -18,8 +24,43 @@ def pagina_entrar(page:Page):
         size=100
     )
     
-    container_valores = cria_container(
-        None
+    entrada_email = cria_container_entrada(
+        "_E-mail", "Jersey10"
+    )
+    
+    entrada_senha = cria_container_entrada(
+        "_Password", "Jersey10"
+    )
+    
+    login_botao = cria_botao(
+        "Login", None
+    )
+    login_botao.width = 220
+    login_botao.height = 60
+    login_botao.content.size = 50
+
+    
+    voltar_botao = cria_botao(
+        "Voltar", lambda e: voltar_pagina_inicial(page)
+    )
+    voltar_botao.width = 200
+    voltar_botao.height = 60
+    voltar_botao.content.size = 50
+    voltar_botao.style.overlay_color = "#971717"
+    
+    container_valores = cria_container_elementos(
+        Column(
+            controls=[
+                entrada_email,
+                entrada_senha,
+                Row(
+                    [login_botao, voltar_botao],
+                    alignment=MainAxisAlignment.CENTER
+                )
+                ],
+            spacing=40,
+            auto_scroll=ScrollMode.AUTO
+        )
     )
     
     page.add(
