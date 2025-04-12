56a7a67710ec18997de114e2004a04538d68428d
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-12 19:38:52 -0300

    docs: documentacao da pagina de login

--- pages/entrar_page.py
+++ pages/entrar_page.py
@@ -6,6 +6,12 @@ from .assets.tools.conv import (cria_container_elementos,
 
 from .fluxo import voltar_pagina_inicial 
 
+"""
+Script com pagina de login.
+Aparece ao clicar botão entrar da
+página inicial.
+"""
+
 def pagina_entrar(page:Page):
     page.title = "Chronos+"
     page.horizontal_alignment = MainAxisAlignment.CENTER
@@ -17,13 +23,14 @@ def pagina_entrar(page:Page):
         "Jersey25": "pages/assets/fonts/Jersey25-Regular.ttf"
     }
     
-    
+    # Texto da página de entrar
     entrar_name = Text(
         value="_Entrar",
         font_family="Jersey15",
         size=100
     )
     
+    # Containers de entrada
     entrada_email = cria_container_entrada(
         "_E-mail", "Jersey10"
     )
@@ -32,6 +39,7 @@ def pagina_entrar(page:Page):
         "_Password", "Jersey10"
     )
     
+    # Botões de Login e Voltar
     login_botao = cria_botao(
         "Login", None
     )
@@ -48,6 +56,7 @@ def pagina_entrar(page:Page):
     voltar_botao.content.size = 50
     voltar_botao.style.overlay_color = "#971717"
     
+    # Container principal, que porta entradas
     container_valores = cria_container_elementos(
         Column(
             controls=[
@@ -63,6 +72,7 @@ def pagina_entrar(page:Page):
         )
     )
     
+    # Adição da página
     page.add(
        Row(
            controls=[entrar_name],
