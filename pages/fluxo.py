e01c35977e4f277997291be79e73708fc0d5c9cd
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-12 14:14:21 -0300

    att: atualizacao dos diretorios + add: fluxo.py def go_entrar

---
+++ pages/fluxo.py
@@ -0,0 +1,11 @@
+from flet import Page
+from .entrar_page import pagina_entrar
+
+"""
+Esse arquivo controla o fluxo entre as páginas
+"""
+
+def go_entrar(page: Page):
+    page.clean()
+    pagina_entrar(page=page)
+    
