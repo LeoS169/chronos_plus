d38704a89738b41e4e560afe30498b0a68367a9e
LeoS169 <leonamfrancisco7@gmail.com>
2025-04-17 14:26:33 -0300

    feat: func para manipular userinfo.json

--- pages/assets/tools/defUserinfo.py
+++ pages/assets/tools/defUserinfo.py
@@ -1,7 +1,22 @@
-import json
+from json import dump
 
 """
 Esse arquivo contém funcoes
 para atualização de informações
 do userinfo.json
 """
+
+def define_userinfo(
+    user:tuple, # format "(a, b,)"
+    qntDiario:int,
+    diarioAtivo:str # nome do diario
+):
+    with open("pages/userinfo.json", "w") as f:
+        dump(
+            {
+                "user": f"{user}",
+                "qntDiario": qntDiario,
+                "diarioAtivo": diarioAtivo
+            }, f
+        )
+    return None
